49a2c8491b45c52c1e377ba32392ece0f35151b5
Priyansh Neel <neelpriyansh@gmail.com>
2025-07-16 23:56:46 +0530

    Start

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -4,11 +4,10 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-    <h1 className='text-2xl'>Priyansh Neel</h1>
+    <h1 className='text-2xl'>Priynsh Neel</h1>
     </>
   )
 }
