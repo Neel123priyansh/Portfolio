13cac64f6aad89afbd7e006fc96b67a67699c255
Priyansh Neel <neelpriyansh@gmail.com>
2025-07-17 18:30:45 +0530

    UI/UX

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,16 +1,19 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import { Route } from 'react-router-dom'
+import { Route, BrowserRouter, Routes } from 'react-router-dom'
 import './App.css'
 
 function App() {
 
   return (
     <>
-    <Route>,
-
-    </Route>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/'element={<Home/>}/>
+      </Route>
+    </Routes>
+    </BrowserRouter>
     </>
   )
 }
