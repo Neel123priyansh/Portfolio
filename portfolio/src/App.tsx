9bee96e34b277f1f10961b76c2eaeb6bb8af999a
Priyansh Neel <neelpriyansh@gmail.com>
2025-07-17 18:27:49 +0530

    UI/UX

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,13 +1,16 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { Route } from 'react-router-dom'
 import './App.css'
 
 function App() {
 
   return (
     <>
-    <h1 className='text-center'>Priyansh</h1>
+    <Route>,
+
+    </Route>
     </>
   )
 }
