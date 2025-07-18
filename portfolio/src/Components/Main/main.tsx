07bff88da4788d7cbd152b2cad691d64fa891da5
Priyansh Neel <neelpriyansh@gmail.com>
2025-07-18 17:35:48 +0530

    UI/UX

--- portfolio/src/Components/Main/main.tsx
+++ portfolio/src/Components/Main/main.tsx
@@ -3,7 +3,9 @@ import "../../index.css";
 export default function Main() {
   return (
     <div className='flex flex-row px-10 justify-between'>
-        <h1 style={{fontFamily: "Viaoda Libre"}} className="text-7xl tracking-wide font-black mt-[220px]">Hi there! I'm Priyansh Neel<br/>web developer, product designer, <br/> maker, & future entrepreneur</h1>
+        <h1 style={{ fontFamily: "Viaoda Libre" }} className="text-7xl tracking-wide font-black mt-[220px]"> Hi there! I'm <span style={{ color: "#b02e2e" }}>Priyansh Neel</span><br /> web developer, product designer, <br />
+  maker, & future entrepreneur
+</h1>
     </div>
   )
 }
