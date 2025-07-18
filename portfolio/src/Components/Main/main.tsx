f481324d337e9fba705f6eff1dc7f53879c1ba84
Priyansh Neel <neelpriyansh@gmail.com>
2025-07-18 15:22:39 +0530

    UI/UX

--- portfolio/src/Components/Main/main.tsx
+++ portfolio/src/Components/Main/main.tsx
@@ -1,9 +1,9 @@
-import "./index.css"; 
+import "../../index.css"; 
 
 export default function Main() {
   return (
-    <div className='flex flex-row justify-between'>
-        <h1 className="text-3xl font-black font- mt-[220px]">Hi there! I'm Priyansh Neel───product designer, maker, & sketcher</h1>
+    <div className='flex flex-row px-10 justify-between'>
+        <h1 style={{ fontFamily: "Regina"}} className="text-5xl tracking-wider font-black mt-[220px]">Hi there! I'm Priyansh Neel<br/>product designer, maker, & sketcher</h1>
     </div>
   )
 }
