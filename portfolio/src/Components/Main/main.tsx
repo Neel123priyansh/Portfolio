5dbc9601833907758b3fdc29fe97daf34560e08e
Priyansh Neel <neelpriyansh@gmail.com>
2025-07-18 20:52:31 +0530

    UI/UX

--- portfolio/src/Components/Main/main.tsx
+++ portfolio/src/Components/Main/main.tsx
@@ -1,108 +1,21 @@
 import "../../index.css"; 
 import Terminal, { ColorMode, TerminalOutput } from 'react-terminal-ui';
+import {
+  AnimatedSpan,
+  Terminal,
+  TypingAnimation,
+} from "../magicui/terminal";
 
 export default function Main() {
   return (
     <div className='flex flex-row px-10 justify-between'>
         <h1 style={{ fontFamily: "Viaoda Libre" }} className="text-7xl tracking-wide font-black mt-[220px]"> Hi there! I'm <span style={{ color: "#b02e2e" }}>Priyansh Neel</span><br /> web developer,<br/> product designer,<br/>maker & future entrepreneur</h1>
+        <Terminal>
+          <TypingAnimation>
+            <AnimatedSpan>Hello, world!</AnimatedSpan>
+            <TypingAnimation>MagicUI is awesome!</TypingAnimation>
+          </TypingAnimation>
+        </Terminal>
     </div>
   )
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
