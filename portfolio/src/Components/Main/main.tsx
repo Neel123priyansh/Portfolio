0be6531b68753cacbce78ba195044b889f3e0d1b
Priyansh Neel <neelpriyansh@gmail.com>
2025-07-18 10:03:09 +0530

    UI/UX

---
+++ portfolio/src/Components/Main/main.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Main() {
+  return (
+    <div className='flex flex-row justify-between'>
+        <h1>Hi there</h1>
+    </div>
+  )
+}
--- portfolio/src/Home/home.tsx
+++ portfolio/src/Home/home.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import Header from '../Components/Header/header'
+import Main from '../Components/Main/main'
 
 export const Home = () => {
   return (
-    <Header/>
+    <>
+    <Header />
+    <Main />
+    </>
   )
 }
 
