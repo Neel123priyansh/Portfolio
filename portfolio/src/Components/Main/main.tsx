fd5783bae6e0216bf14e3e5e6cb1f8050cf26beb
Priyansh Neel <neelpriyansh@gmail.com>
2025-07-18 17:58:32 +0530

    UI/UX

--- portfolio/src/Components/Main/main.tsx
+++ portfolio/src/Components/Main/main.tsx
@@ -1,11 +1,10 @@
 import "../../index.css"; 
+import Terminal, { ColorMode, TerminalOutput } from 'react-terminal-ui';
 
 export default function Main() {
   return (
     <div className='flex flex-row px-10 justify-between'>
-        <h1 style={{ fontFamily: "Viaoda Libre" }} className="text-7xl tracking-wide font-black mt-[220px]"> Hi there! I'm <span style={{ color: "#b02e2e" }}>Priyansh Neel</span><br /> web developer, product designer, <br />
-  maker, & future entrepreneur
-</h1>
+        <h1 style={{ fontFamily: "Viaoda Libre" }} className="text-7xl tracking-wide font-black mt-[220px]"> Hi there! I'm <span style={{ color: "#b02e2e" }}>Priyansh Neel</span><br /> web developer,<br/> product designer,<br/>maker & future entrepreneur</h1>
     </div>
   )
 }
