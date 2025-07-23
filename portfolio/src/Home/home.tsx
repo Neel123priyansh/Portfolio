82a1797bd0266eaf7d8e1d2872495aaa8667c6f0
Priyansh Neel <neelpriyansh@gmail.com>
2025-07-23 22:37:48 +0530

    UI/UX

--- portfolio/src/Home/home.tsx
+++ portfolio/src/Home/home.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from '../Components/Header/header'
 import Main from '../Components/Main/main'
+import { SmoothCursor } from "../Components/ui/smooth-cursor";
 import Move from './move'
 
 export const Home = () => {
   return (
     <>
+    <SmoothCursor />
     <Header/>
     <Main/>
     <Move/>
