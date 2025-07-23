432a43a0b18c834856ec8c04bf7ab1b87c266dbf
Priyansh Neel <neelpriyansh@gmail.com>
2025-07-23 12:11:52 +0530

    UI/UX

--- portfolio/src/Components/Main/main.tsx
+++ portfolio/src/Components/Main/main.tsx
@@ -9,7 +9,7 @@ import { WordRotate } from "../magicui/word-rotate";
 export default function Main() {
   return (
     <div className='flex flex-row px-10 justify-between'>
-        <h1 style={{ fontFamily: "Viaoda Libre" }} className="text-7xl tracking-wide font-black mt-[220px]"> Hi there! I'm <span style={{ color: "#b02e2e" }}>Priyansh Neel</span><br /> web developer,<br/> product designer,<br/>maker & future entrepreneur</h1>
+        <h1 style={{ fontFamily: "Viaoda Libre" }} className="text-7xl tracking-wide font-black mt-[220px]"> Hi there! I'm <WordRotate words={["Word", "Rotate"]} /><br /> web developer,<br/> product designer,<br/>maker & future entrepreneur</h1>
             <Terminal className="mt-[160px] bg-gray-900">
       <TypingAnimation className="text-blue-50">&gt; pnpm dlx shadcn@latest init</TypingAnimation>
  
