cf2506fd5e7328b8faf06b190b72f5ab6fe7fc2c
Priyansh Neel <neelpriyansh@gmail.com>
2025-07-23 22:27:08 +0530

    UI/UX

--- portfolio/src/Home/move.tsx
+++ portfolio/src/Home/move.tsx
@@ -3,7 +3,7 @@ import "../index.css";
 
 export default function Move() {
   return (
-    <div className="flex flex-col items-center font-robotocondensed text-xl tracking-[0.25em] justify-center mt-15 animate-collapsible-up">
+    <div className="flex flex-col items-center font-robotocondensed text-xl tracking-[0.25em] justify-center mt-15 animate-bounce">
         <h1>SELECTED WORKS</h1>
         <h1>↓</h1>
     </div>
