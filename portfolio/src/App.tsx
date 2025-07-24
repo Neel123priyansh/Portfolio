86134b79918e23405c4bdf21b497fb0f4c67b171
Priyansh Neel <neelpriyansh@gmail.com>
2025-07-24 22:54:28 +0530

    UI/UX

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import Home from './Home/home'
 import { Route, BrowserRouter, Routes } from 'react-router-dom'
 import './App.css'
