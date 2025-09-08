9693ee7ef1b093010d9f6c5d6a3bb3b2fe048ae9
Neel123priyansh <neelpriyansh@gmail.com>
2025-09-08 23:51:17 +0530

    Again Change

--- portfolio/src/Components/Header/header.tsx
+++ portfolio/src/Components/Header/header.tsx
@@ -4,7 +4,7 @@ export default function header() {
   
   return (
     <div className='fixed z-50 w-full backdrop-blur-lg px-10 bg-white/5 border-white/1 flex justify-between items-center max-w-[100%] text-white h-24 mx-auto'>    
-        <h1 onClick={() => navigate('/Check')} className='text-3xl font-robotocondensed tracking-[0.25em] text-[#252b29]'>PRIYANSH NEEL</h1>      
+        <h1 onClick={() => navigate('/Check')} className='text-3xl font-robotocondensed tracking-[0.25em] text-[#252b29]'>PRIYANSH NEEL</h1>       
         <ul className='flex flex-row text-xl justify-between text-[#252b29] gap-10 font-robotocondensed tracking-widest '>   
           <li className='p-4'>PORTFOLIO</li>     
           <li className='p-4'>ABOUT</li>           
